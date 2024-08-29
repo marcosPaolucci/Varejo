daa47fbd476408b07cd1d09af54a583554c46290
marcosPaolucci <marcospaolucci_s@yahoo.com.br>
2024-08-29 14:47:57 -0300

    subclasses

--- produto.py
+++ produto.py
@@ -29,6 +29,18 @@ class Produto:
 
 #OBS: podemos criar categoria eletronicos por exemplo como uma classe filha de produtos
 
+class Eletrodomesticos(Produto):
+    def __init__(self, codigo, nome, quantidade, preço, descricao, fornecedor, voltagem):
+        self.categoria = "Eletronicos"
+        super().__init__(codigo, nome, self.categoria ,quantidade, preço, descricao, fornecedor)
+        self.voltagem = voltagem
+
+class Roupas(Produto):
+    def __init__(self, codigo, nome, quantidade, preço, descricao, fornecedor, tamanho):
+        self.categoria = "Eletronicos"
+        super().__init__(codigo, nome, self.categoria ,quantidade, preço, descricao, fornecedor)
+        self.tamanho = tamanho
+
 tenis = Produto(1, "tenis de corrida", "calçado", 10, 500, "bom tenis", "Paraguai")
 tenis.adicionar_quantidade()
 print(tenis.quantidade)
@@ -42,3 +54,17 @@ print(tenis.quantidade)
 tenis.atualizar_quantidade(-6)
 tenis.atualizar_preço(600)
 print(tenis.preço)
+
+# Exemplo de uso
+tv = Eletrodomesticos("001", "TV", 10, 1500, "TV 4K", "Samsung", "110V")
+secador = Eletrodomesticos("002", "Secador", 50, 100, "Secador bivolt", "Arco", "110/220V")
+camiseta = Roupas("003", "Camiseta Blueprint", 20, 70, "Camiseta azul listrada", "Lacoste", "G")
+print(tv.nome)        # TV
+print(tv.categoria)   # Eletronicos
+print(tv.voltagem)    # 220V
+tv.adicionar_quantidade()
+print(tv.quantidade)
+camiseta.remover_quantidade()
+print(camiseta.quantidade)
+print(secador.descricao)
+print(camiseta.tamanho)

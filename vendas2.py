55d3935b0d3fba543e747895a3106d6230cb4b28
marcosPaolucci <marcospaolucci_s@yahoo.com.br>
2024-09-24 11:57:20 -0300

    MongofaltaFornec

---
+++ vendas2.py
@@ -0,0 +1,54 @@
+from estoque import get_produto
+
+def Registrar_Venda(CPFcliente, codigoProdutos, quantidades, promoção=None):
+    # Inicializa o total
+    total = 0
+    itens_venda = []
+
+    # Percorre os produtos e suas quantidades
+    for codigo, quantidade in zip(codigoProdutos, quantidades):
+        produto = get_produto(codigo)  # Busca o produto pelo código diretamente do MongoDB
+        if produto:
+            preco = produto.preço  # Obtém o preço do produto
+            if promoção:
+                if codigo == promoção[0]:
+                    preco = preco * promoção[1]
+            quantidade_estoque = produto.quantidade
+            if quantidade_estoque < quantidade:
+                print(f"Quantidade insuficiente no estoque para o produto: {produto.nome}")
+                return
+            subtotal = preco * quantidade
+            total += subtotal
+            itens_venda.append({
+                "codigo": codigo,
+                "nome": produto.nome,
+                "quantidade": quantidade,
+                "preco_unitario": preco,
+                "subtotal": subtotal
+            })
+        else:
+            print(f"Produto com código {codigo} não encontrado no estoque.")
+            return 
+    
+    # Atualiza a quantidade de produtos no estoque
+    for item in itens_venda:
+        produto = get_produto(item["codigo"])
+        quantidade = item["quantidade"]
+        for i in range(quantidade):
+            produto.remover_quantidade()  # Atualiza a quantidade diretamente no MongoDB
+
+    print("Venda registrada com sucesso!")
+    
+    # Retorna um dicionário com os dados da venda
+    nota_fiscal = {
+        "CPFcliente": CPFcliente,
+        "itens": itens_venda,
+        "total": total
+    }
+    return nota_fiscal
+
+venda = Registrar_Venda("12345678900", [1], [2], [1,0.5])  # Código 1 (Geladeira), Código 2 (A Bela Adormecida)
+print(venda["total"])  # Imprime o total da venda
+print(venda["itens"])
+print(venda["CPFcliente"])
+
